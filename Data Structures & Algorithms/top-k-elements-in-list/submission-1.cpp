2ab665f4aa5b491b8f6b5a735cffb5eb15d296f7
MDP2004 <146109756+MDP2004@users.noreply.github.com>
2026-05-23 12:31:09 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    static bool comp (pair<int,int> a, pair<int,int> b){
+        return a.second < b.second;
+    }
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map <int,int> hash;
+
+        for (int num : nums) hash[num]++;
+
+        priority_queue <pair<int,int> , vector<pair<int,int>>> s;
+
+        for (auto p : hash){
+            s.push({p.second, p.first});
+        }
+        vector<int> ans;
+        while (k--){
+            ans.push_back(s.top().second);
+            s.pop();
+        }
+        return ans;
+    }   
+
+};
