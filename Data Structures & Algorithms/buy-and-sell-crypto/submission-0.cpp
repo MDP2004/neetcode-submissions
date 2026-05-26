8e2bab2257cd95229a69bda051e4bf05a213b715
MDP2004 <146109756+MDP2004@users.noreply.github.com>
2026-05-26 23:15:18 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int currMax = prices[prices.size()-1];
+        for (int i = prices.size()-1 ; i>=0 ; i--){
+            currMax = max(currMax , prices[i]);
+            ans = max(ans, currMax-prices[i]);
+        }
+        return ans;
+    }
+};
